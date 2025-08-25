74c2791ebc4538c0e41d130c9f5515acaa246073
zoieleung <zoieleung309@gmail.com>
2025-08-25 16:48:48 +1000

    test

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.211.134.86:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://13.211.134.86:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
